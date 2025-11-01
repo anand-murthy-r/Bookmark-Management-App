134160191ceaf03a8db00449e13bfe3e4310a9f5
anand-murthy-r <anu.2004.mys@gmail.com>
2025-11-01 19:58:22 +0530

    readme

--- bookmark-management/src/main.ts
+++ bookmark-management/src/main.ts
@@ -9,7 +9,7 @@ async function bootstrap() {
   app.enableCors({
       origin: '*',
       methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
-      credentials: true
+      allowedHeaders: ['Content-Type', 'Authorization']
   });
   await app.listen(process.env.PORT ?? 3000, '0.0.0.0');
 }
